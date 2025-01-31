f5179d5b3f42c6c17b6c09e562c0e801f26c952e
Leonardo Salazar <leosalazarv18@gmail.com>
2025-01-31 20:48:25 +0300

    Objetos en JavaScript

---
+++ Introducción a Objetos en JavaScript.js
@@ -0,0 +1,23 @@
+const auto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2022,
+    mostrarInfo: function() {
+      console.log(`Auto: ${this.marca} Modelo: ${this.modelo}, Año: ${this.año}`);
+    }
+  };
+  
+  auto.mostrarInfo();
+
+
+const libro = {
+    titulo : "La Metamorphosis",
+    autor : "Kafka",
+    anio : "1915",
+    describirLibro : function() {
+        console.log( `Libro Titulado: ${this.titulo} escrito por ${this.autor} en el anio de ${this.anio}` ) ;
+    }
+
+}
+
+libro.describirLibro();
