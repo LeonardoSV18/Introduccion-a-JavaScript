02010dbd5edf8fee932e7b016bd9586b34460c12
Leonardo Salazar <leosalazarv18@gmail.com>
2025-02-19 16:43:28 +0300

    Funciones, ejercicios de clase y entregables

--- scripts/Funciones en JavaScript.js
+++ scripts/Funciones en JavaScript.js
@@ -19,3 +19,67 @@ function mostrarLibrosLeidos(){
 
 agregarLibro(NuevoLibro);
 mostrarLibrosLeidos();
+
+
+/* Ejercicios de clase 
+
+function anosParaElRetiro(edad){
+    console.log(65 - edad);
+}
+
+anosParaElRetiro(21); 
+
+
+function EsTarde(hora) {
+
+    if (hora>12 ) {
+        return "Es Tarde";
+    }
+
+    else {
+        return "Es Temprano"; 
+    }
+}
+
+
+    console.log(EsTarde(13));
+
+
+function Hola(nombre){
+    console.log("Hola,",nombre);
+}
+
+Hola("Leonardo")
+
+
+function factorial(num){ 
+    if(num <= 1) {
+        return 1; 
+    }
+    else {
+        return num * factorial (num-1); 
+    }
+
+}
+
+console.log(factorial(5));
+
+
+function BubbleSort(arr){
+    let n = arr.length;
+    for(let i=0; i < n; i++)
+        for(let j=0; j < n - i - 1; j++){
+            if(arr[j] < arr[j + 1]){
+                let temp = arr[j]; 
+                arr[j] = arr[j+1];
+                arr[j+1] = temp; 
+            }
+        }
+            
+        return arr;
+}
+
+console.log(BubbleSort([5, 3, 8, 1, 2, 7, 4, 6 ])); 
+
+*/ 
+
