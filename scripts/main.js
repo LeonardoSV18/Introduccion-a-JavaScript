66f9ea2cb20b81237cfc61cfffc91a058cd7b7b1
Leonardo Salazar <leosalazarv18@gmail.com>
2025-03-07 08:51:51 +0300

    Fecha  y hora añadida en el JS

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,12 @@ const MessageForm = document.querySelector("#Message-Form");
 const handleUploadForm = (e) => {
     e.preventDefault();
     const UploadMessage = document.querySelector("#Message-Form input").value;
-    addMessage(UploadMessage);
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+    addMessage(UploadMessage, currentDateTime);
 }
 
-const addMessage = (text) => {
+const addMessage = (text, time) => {
     const container = document.querySelector(".CommentContainer");
 
 
@@ -14,12 +16,14 @@ const addMessage = (text) => {
         <div class="CommentContainer">
         <div class="MSN">
             <h4>${text}</h4>
+            <h6>${time}</h6>
             <button>Eliminar</button>
         </div>
+
     ` )
 
     const MSNs = Array.from(document.querySelectorAll(".MSN"));
-    MSNs[MSNs.length - 1].children[1].addEventListener('click', (e) => deleteMSN(e));
+    MSNs[MSNs.length - 1].children[2].addEventListener('click', (e) => deleteMSN(e));
 }
 
 const deleteMSN = (e) => {
@@ -27,5 +31,6 @@ const deleteMSN = (e) => {
     clickedElement.parentElement.remove();
 
 }
+
 
 MessageForm.addEventListener('submit', (e) => handleUploadForm(e));
